fa4b23d9add5bf26e2c10156c99e610806ed1819
duandawei <76507996+mirrorlifedoc@users.noreply.github.com>
2024-05-26 09:43:17 +0900

    commit
    commit

--- src/main/java/com/example/entity/Facility.java
+++ src/main/java/com/example/entity/Facility.java
@@ -1,9 +1,12 @@
 package com.example.entity;
 
-import lombok.Data;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
 @Data
